2890c542957e2f0942d6d561a2582c3ff929fe72
面向对象方法学 <961111759@qq.com>
2015-11-03 23:18:35 +0800

    Update easyxCar.cpp

--- third/easyxCar.cpp
+++ third/easyxCar.cpp
@@ -1,4 +1,4 @@
-// easyxCar.cpp : ¶¨Òå¿ØÖÆÌ¨Ó¦ÓÃ³ÌÐòµÄÈë¿Úµã¡£
+// easyxCar.cpp : å®šä¹‰æŽ§åˆ¶å°åº”ç”¨ç¨‹åºçš„å…¥å£ç‚¹ã€‚
 //
 
 #include "stdafx.h"
@@ -12,21 +12,20 @@ using namespace std;
 
 
 
-class Figure//¹¹ÔìÒ»¸ö»­Í¼»ùÀà
+class Figure//æž„é€ ä¸€ä¸ªç”»å›¾åŸºç±»
 {
 public:
-	virtual void Draw() {}
-	virtual void Hide() {}
-	virtual void Move() {}
+	virtual void Draw() {};
+	virtual void Hide() {};
+	virtual void Move() {};
 }; 
 
-class Circle :public Figure//¹¹ÔìÔ²Àà
+class Circle :public Figure//æž„é€ åœ†ç±»
 {
 private:
-	int x, y;//Ô²µÄ×ø±ê
-	int radius;//Ô²µÄ°ë¾¶
+	int x, y;//åœ†çš„åæ ‡
+	int radius;//åœ†çš„åŠå¾„
 public:
-	Circle(){}
 	Circle(int nx, int ny, int nr)
 	{
 		x = nx;
@@ -46,9 +45,9 @@ public:
 	{
 		clearcircle(x, y, radius + 1);
 	}
-};//¹¹ÔìÔ²Àà
+};//æž„é€ åœ†ç±»
 
-class Rectangles :public Figure//¹¹ÔìËÄ±ßÐÎÀà
+class Rectangles :public Figure//æž„é€ å››è¾¹å½¢ç±»
 {
 private:
 	int left, right, top, bottom;
@@ -77,9 +76,9 @@ public:
 	{
 		clearrectangle(left, top, right, bottom);
 	}
-};//¹¹ÔìËÄ±ßÐÎÀà//¹¹ÔìËÄ±ßÐÎÀà
+};//æž„é€ å››è¾¹å½¢ç±»//æž„é€ å››è¾¹å½¢ç±»
 
-class Triangle :public Figure//»­Èý½ÇµÄÀà
+class Triangle :public Figure//ç”»ä¸‰è§’çš„ç±»
 {
 private:
 	int px;
@@ -118,33 +117,22 @@ public:
 class FigureLink
 {
 public:
-	Figure *node;
+	Figure *node[10];
 	int size;
-	FigureLink(){}
-	void setFiureLink(int s)
+	FigureLink():size(0){}
+	void Insert(Figure *a)
 	{
-		size = s;
-		node = new Figure[size];
-	}
-	FigureLink(int size) :size(size)
-	{
-		node = new Figure[size];
-	}
-	FigureLink(FigureLink *p)
-	{
-		node = p->node;
-	}
-	void Insert(Figure *a,int i)
-	{
-		node[i] = *a;
+		node[size++] = a;
 	}
 };
+
 class vechiel
 {public:
-	FigureLink *contents;
-	void Draw(){}
-	void Move(int dx,int dy){}
-	void Hide(){}
+	FigureLink contents;
+	virtual void Draw(){}
+	virtual void Move(int dx,int dy){}
+	virtual void Hide(){}
+	virtual void Defaultvechiel(){}
 };
 
 class Car :public vechiel
@@ -153,51 +141,95 @@ private:
 	int wheel_size;
 	int xstart;
 	int ystart;
+	int origin_x;
+	int origin_y;
 public:
-	
-	Car(int w, int x, int y)
+	Car(int x, int y, int w) :origin_x(x), origin_y(y)
 	{
-		contents->setFiureLink(6);
 		wheel_size = w;
 		xstart = x;
 		ystart = y;
-		Circle c1(xstart, ystart, wheel_size);
-		Circle c2(xstart + 9 * wheel_size, ystart, wheel_size);
+		Circle c1=Circle(xstart, ystart, wheel_size); 
+		Circle c2= Circle(xstart + 9 * wheel_size, ystart, wheel_size);
 		Figure *a = &c1;
 		Figure *b = &c2;
-		contents->Insert(a,0);
-		contents->Insert(b,1);
-		Rectangles rec1(xstart - 3.5*wheel_size, ystart - 3 * wheel_size, xstart + 12.5*wheel_size, ystart - wheel_size);
-		Rectangles rec2(xstart + wheel_size, ystart - 5 * wheel_size, xstart + 8 * wheel_size, ystart - 3 * ystart);
+		contents.Insert(a); 
+		contents.Insert(b); 
+		Rectangles rec1=Rectangles(xstart - 3.5*wheel_size, ystart - 3 * wheel_size, xstart + 12.5*wheel_size, ystart - wheel_size);
+		Rectangles rec2=Rectangles(xstart + wheel_size, ystart - 5 * wheel_size, xstart + 8 * wheel_size, ystart - 3 * wheel_size);
 		Figure *c = &rec1;
 		Figure *d = &rec2;
-		contents->Insert(c,2);
-		contents->Insert(d,3);
-		Triangle t1(xstart + wheel_size, ystart - 5 * wheel_size, xstart - wheel_size, ystart - 3 * wheel_size);
-		Triangle t2(xstart + 8 * wheel_size, ystart - 5 * wheel_size, xstart + 10 * wheel_size, ystart - 3 * wheel_size);
-		Figure *e = &t1;
+		contents.Insert(c); 
+		contents.Insert(d); 
+		Triangle t1=Triangle(xstart + wheel_size, ystart - 5 * wheel_size, xstart - wheel_size, ystart - 3 * wheel_size);
+		Triangle t2=Triangle(xstart + 8 * wheel_size, ystart - 5 * wheel_size, xstart + 10 * wheel_size, ystart - 3 * wheel_size);
+		Figure *e =& t1;
 		Figure *f = &t2;
-		contents->Insert(e,4);
-		contents->Insert(f,5);
+		contents.Insert(e);
+		contents.Insert(f); 
 	}
+
 	void Draw()
 	{
-		for (int i = 0; i < 6; i++)
-			contents->node->Draw();
+		contents.size = 0;
+		Circle c1 = Circle(xstart, ystart, wheel_size);
+		Circle c2 = Circle(xstart + 9 * wheel_size, ystart, wheel_size);
+		Figure *a = &c1;
+		Figure *b = &c2;
+		contents.Insert(a);
+		contents.Insert(b);
+		Rectangles rec1 = Rectangles(xstart - 3.5*wheel_size, ystart - 3 * wheel_size, xstart + 12.5*wheel_size, ystart - wheel_size);
+		Rectangles rec2 = Rectangles(xstart + wheel_size, ystart - 5 * wheel_size, xstart + 8 * wheel_size, ystart - 3 * wheel_size);
+		Figure *c = &rec1;
+		Figure *d = &rec2;
+		contents.Insert(c);
+		contents.Insert(d);
+		Triangle t1 = Triangle(xstart + wheel_size, ystart - 5 * wheel_size, xstart - wheel_size, ystart - 3 * wheel_size);
+		Triangle t2 = Triangle(xstart + 8 * wheel_size, ystart - 5 * wheel_size, xstart + 10 * wheel_size, ystart - 3 * wheel_size);
+		Figure *e = &t1;
+		Figure *f = &t2;
+		contents.Insert(e);
+		contents.Insert(f);
+		for (int i = 0; i < contents.size; i++)
+			contents.node[i]->Draw();
 	}
 	void Hide()
 	{
-		for (int i = 0; i < 6; i++)
-			contents->node->Hide();
+		contents.size = 0;
+		Circle c1 = Circle(xstart, ystart, wheel_size);
+		Circle c2 = Circle(xstart + 9 * wheel_size, ystart, wheel_size);
+		Figure *a = &c1;
+		Figure *b = &c2;
+		contents.Insert(a);
+		contents.Insert(b);
+		Rectangles rec1 = Rectangles(xstart - 3.5*wheel_size, ystart - 3 * wheel_size, xstart + 12.5*wheel_size, ystart - wheel_size);
+		Rectangles rec2 = Rectangles(xstart + wheel_size, ystart - 5 * wheel_size, xstart + 8 * wheel_size, ystart - 3 * wheel_size);
+		Figure *c = &rec1;
+		Figure *d = &rec2;
+		contents.Insert(c);
+		contents.Insert(d);
+		Triangle t1 = Triangle(xstart + wheel_size, ystart - 5 * wheel_size, xstart - wheel_size, ystart - 3 * wheel_size);
+		Triangle t2 = Triangle(xstart + 8 * wheel_size, ystart - 5 * wheel_size, xstart + 10 * wheel_size, ystart - 3 * wheel_size);
+		Figure *e = &t1;
+		Figure *f = &t2;
+		contents.Insert(e);
+		contents.Insert(f);
+		for (int i = 0; i < contents.size; i++)
+			contents.node[i]->Hide();
 	}
 	void Move(int dx,int dy)
 	{
+		
+		Hide();
 		xstart = dx;
 		ystart = dy;
-		Hide();
 		Draw();
 	}
-	
+	void Defaultvechiel()
+	{
+		xstart = origin_x;
+		ystart = origin_y;
+	}
 };
 
 
@@ -207,63 +239,95 @@ private:
 	int wheel_size;
 	int xstart;
 	int ystart;
+	int origin_x;
+	int origin_y;
 public:
-	Truck(int w, int x, int y)
+	Truck(int x, int y, int w) :origin_x(x), origin_y(y)
 	{
-		contents->setFiureLink(7);
 		wheel_size = w;
 		xstart = x;
 		ystart = y;
-		Circle c1(xstart, ystart, wheel_size);
-		Circle c2(xstart+2.5*wheel_size,ystart,wheel_size);
-		Circle c3(xstart+11.5*wheel_size,ystart,wheel_size);
-		Circle c4(xstart+14*wheel_size,ystart,wheel_size);
-		Circle c5(xstart+20*wheel_size+2,ystart,wheel_size);
+	}
+	void Draw()
+	{
+		contents.size = 0;
+		Circle c1 = Circle(xstart, ystart, wheel_size);
+		Circle c2 = Circle(xstart + 2.5*wheel_size, ystart, wheel_size);
+		Circle c3 = Circle(xstart + 11.5*wheel_size, ystart, wheel_size);
+		Circle c4 = Circle(xstart + 14 * wheel_size, ystart, wheel_size);
+		Circle c5 = Circle(xstart + 20 * wheel_size + 2, ystart, wheel_size);
 		Figure *a = &c1;
 		Figure *b = &c2;
 		Figure *c = &c3;
 		Figure *d = &c4;
 		Figure *e = &c5;
-		contents->Insert(a,0);
-		contents->Insert(b,1);
-		contents->Insert(c,2);
-		contents->Insert(d,3);
-		contents->Insert(e,4);
-		Rectangles rec1(xstart-2*wheel_size,ystart-9*wheel_size,xstart+16*wheel_size,ystart-wheel_size);
-		Rectangles rec2(xstart+16*wheel_size+2,ystart-7*wheel_size,xstart+20*wheel_size+2,ystart-wheel_size);
+		contents.Insert(a);
+		contents.Insert(b);
+		contents.Insert(c);
+		contents.Insert(d);
+		contents.Insert(e);
+		Rectangles rec1(xstart - 2 * wheel_size, ystart - 9 * wheel_size, xstart + 16 * wheel_size, ystart - wheel_size);
+		Rectangles rec2(xstart + 16 * wheel_size + 2, ystart - 7 * wheel_size, xstart + 20 * wheel_size + 2, ystart - wheel_size);
 		Figure *f = &rec1;
 		Figure *g = &rec2;
-		contents->Insert(f,5);
-		contents->Insert(g,6);
-	}
-	void Draw()
-	{
-		for (int i = 0; i < 7; i++)
-			contents->node->Draw();
+		contents.Insert(f);
+		contents.Insert(g);
+		for (int i = 0; i < contents.size; i++)
+			contents.node[i]->Draw();
 	}
 	void Hide()
 	{
-		for (int i = 0; i < 7; i++)
-			contents->node->Hide();
+		contents.size = 0;
+		Circle c1 = Circle(xstart, ystart, wheel_size);
+		Circle c2 = Circle(xstart + 2.5*wheel_size, ystart, wheel_size);
+		Circle c3 = Circle(xstart + 11.5*wheel_size, ystart, wheel_size);
+		Circle c4 = Circle(xstart + 14 * wheel_size, ystart, wheel_size);
+		Circle c5 = Circle(xstart + 20 * wheel_size + 2, ystart, wheel_size);
+		Figure *a = &c1;
+		Figure *b = &c2;
+		Figure *c = &c3;
+		Figure *d = &c4;
+		Figure *e = &c5;
+		contents.Insert(a);
+		contents.Insert(b);
+		contents.Insert(c);
+		contents.Insert(d);
+		contents.Insert(e);
+		Rectangles rec1(xstart - 2 * wheel_size, ystart - 9 * wheel_size, xstart + 16 * wheel_size, ystart - wheel_size);
+		Rectangles rec2(xstart + 16 * wheel_size + 2, ystart - 7 * wheel_size, xstart + 20 * wheel_size + 2, ystart - wheel_size);
+		Figure *f = &rec1;
+		Figure *g = &rec2;
+		contents.Insert(f);
+		contents.Insert(g);
+		for (int i = 0; i < contents.size; i++)
+			contents.node[i]->Hide();
 	}
 	void Move(int dx, int dy)
 	{
+		Hide();
 		xstart = dx;
 		ystart = dy;
-		Hide();
 		Draw();
+	}
+	void Defaultvechiel()
+	{
+		xstart = origin_x;
+		ystart = origin_y;
 	}
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int xstart, ystart, wheel_size;
-	cout << "ÇëÊäÈë³µÂÖµÄ´óÐ¡£¨°ë¾¶£©:" << endl;
-	cin >> wheel_size;
-	cout << "ÇëÊäÈëÆðÊ¼Î»ÖÃ×ø±ê(21<x<780;)£º" << endl;
-	cin >> xstart;
+	int xstart=70, ystart, wheel_size=7;
+	//cout << "è¯·è¾“å…¥è½¦è½®çš„å¤§å°ï¼ˆåŠå¾„ï¼‰:" << endl;
+	//cin >> wheel_size;
+	//cout << "è¯·è¾“å…¥èµ·å§‹ä½ç½®åæ ‡(50<x<780;)ï¼š" << endl;
+	//cin >> xstart;
+	xstart = xstart + wheel_size;
 	ystart = 500 - wheel_size;
 	initgraph(800, 600);
+	//circle(700,500,20);
+	//circle(30,30,20);
 	outtext(_T("1Car   2 Truck   3 Exit"));
 	line(20, 20, 20, 580);
 	line(20, 580, 780, 580);
@@ -284,108 +348,51 @@ int _tmain(int argc, _TCHAR* argv[])
 	graphdefaults();
 	//line()
 	//circle(20, 20, 20);
-	int contral = 0;
-	int choose = 0;
-	int ms = 150;
 	bool pause = false;
-	Car car1(xstart, ystart, wheel_size);
-	Truck truck1(xstart, ystart, wheel_size);
-	vechiel *v = &car1;
-	while (choose != 3)
+	Car car1(xstart, ystart, wheel_size); 
+	Truck truck1(xstart, ystart, wheel_size); 
+	vechiel *v=&truck1;
+	int one = 0;
+	int ms = 1500;
+
+	char two;
+	while (one != '3')
 	{
-		switch (choose)
-		{
-		case 1:
-		{
-				  v = &car1;
-				  v->Draw();
-				  while (contral != 'E' || contral != 'e')
-				  {
-					  switch (contral)
-					  {
-					  case 's':
-					  {
-								  for (int i = 20; i < 700; i++)
-								  {
-									  v->Move(i, ystart);
-									  Sleep(ms);
-								  }
-
-
-					  }
-					  case 'p':
-					  {
-								  pause = !pause;
-								  while (pause == true)
-								  {
-									  int p = _getch();
-									  if (p == 'p') pause = false;
-								  }
-					  }
-					  case '+':
-					  {
-								  ms = ms - 10;
-								  if (ms < 20) ms = 20;
-					  }
-					  case '-':
-					  {
-								  ms = ms + 10;
-								  if (ms>300) ms = 300;
-					  }
-					  }
-					  if (_kbhit())
-						  contral = _getch();
-				  }
-
+		one = _getch();
+		if (one =='1') {
+			v = &car1; v->Defaultvechiel(); v->Draw();
 		}
-		case 2:
+		if (one == '2') {
+			v = &truck1; v->Defaultvechiel(); v->Draw();
+		}
+		if (one == '1'|| one =='2')
 		{
-				  v = &truck1;
-				  v->Draw();
-				  while (contral != 'E' || contral != 'e')
-				  {
-					  switch (contral)
-					  {
-					  case 's':
-					  {
-								  for (int i = 20; i < 700; i++)
-								  {
-									  v->Move(i, ystart);
-									  Sleep(ms);
-								  }
-
-
-					  }
-					  case 'p':
-					  {
-								  pause = !pause;
-								  while (pause == true)
-								  {
-									  int p = _getch();
-									  if (p == 'p') pause = false;
-								  }
-					  }
-					  case '+':
-					  {
-								  ms = ms - 10;
-								  if (ms < 20) ms = 20;
-					  }
-					  case '-':
-					  {
-								  ms = ms + 10;
-								  if (ms>300) ms = 300;
-					  }
-					  }
-					  if (_kbhit())
-						  contral = _getch();
-				  }
-
+			two = _getch();
+			if (two=='s')
+			for (int i = xstart; i < 780; i += 50)
+			{
+				v->Move(i, ystart);
+				Sleep(ms);
+				if (_kbhit()) 
+				{ two = _getch(); 
+				if (two == 'e') { v->Hide(); break; }
+				if (two == 'p')
+				{
+					pause = true;
+					while (pause == true)
+					{
+						two = _getch();
+						if (two == 'p')
+							pause = !pause;
+					}
+				}
+				if (two == '+') ms = ms-200;
+				if (two == '-') ms = ms + 200;
+				}
+			}
+		
 		}
-		}
-		if (_kbhit())
-			choose = _getch();
 	}
-	//_getch();
 	closegraph();
 	return 0;
 }
